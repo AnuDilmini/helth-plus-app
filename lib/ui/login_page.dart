dfd2642d2aa5309dab96cbb3b3da1888ea340ce7
AnushikaDilmini <anushikadilmini@gmail.com>
2020-02-26 17:26:02 +0530

    add files

--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -1,144 +1,271 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:health_plus/animation/fade_animation.dart';
-import 'package:health_plus/ui/registration.dart';
-
-
-class LoginPage extends StatelessWidget {
-  final _phoneController = TextEditingController();
-  final _codeController = TextEditingController();
-
-  Future<bool> loginUser(String phone, BuildContext context) async{
-    FirebaseAuth _auth = FirebaseAuth.instance;
-
-    _auth.verifyPhoneNumber(
-        phoneNumber: phone,
-        timeout: Duration(seconds: 60),
-        verificationCompleted: (AuthCredential credential) async{
-          Navigator.of(context).pop();
-
-          AuthResult result = await _auth.signInWithCredential(credential);
-
-          FirebaseUser user = result.user;
-
-          if(user != null){
-            Navigator.push(context, MaterialPageRoute(
-                builder: (context) => Registration(user: user,)
-            ));
-          }else{
-            print("Error");
-          }
-
-
-          //This callback would gets called when verification is done auto maticlly
-        },
-        verificationFailed: (AuthException exception){
-          print(exception);
-        },
-        codeSent: (String verificationId, [int forceResendingToken]){
-          showDialog(
-              context: context,
-              barrierDismissible: false,
-              builder: (context) {
-                return AlertDialog(
-                  title: Text("Give the code?"),
-                  content: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      TextField(
-                        controller: _codeController,
-                      ),
-                    ],
-                  ),
-                  actions: <Widget>[
-                    FlatButton(
-                      child: Text("Confirm"),
-                      textColor: Colors.white,
-                      color: Colors.blue,
-                      onPressed: () async{
-                        final code = _codeController.text.trim();
-                        AuthCredential credential = PhoneAuthProvider.getCredential(verificationId: verificationId, smsCode: code);
-
-                        AuthResult result = await _auth.signInWithCredential(credential);
-
-                        FirebaseUser user = result.user;
-
-                        if(user != null){
-                          Navigator.push(context, MaterialPageRoute(
-                              builder: (context) => Registration(user: user,)
-                          ));
-                        }else{
-                          print("Error");
-                        }
-                      },
-                    )
-                  ],
-                );
-              }
-          );
-        },
-        codeAutoRetrievalTimeout: null
-    );
-  }
-
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(3, 9, 23, 1),
-      body: Container(
-        padding: EdgeInsets.all(30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FadeAnimation(1.2, Text("Enter Your Mobile Number",
-              style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),)),
-            SizedBox(height: 30,),
-            FadeAnimation(1.5, Container(
-              padding: EdgeInsets.all(10),
-            )),
-
-            TextFormField(
-              decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(8)),
-                      borderSide: BorderSide(color: Colors.grey[200])
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(8)),
-                      borderSide: BorderSide(color: Colors.grey[300])
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey[100],
-                  hintText: "Mobile Number"
-
-              ),
-              controller: _phoneController,
-            ),
-            SizedBox(height: 40,),
-
-
-            Container(
-              width: double.infinity,
-              child: FlatButton(
-                child: Text("LOGIN"),
-                textColor: Colors.white,
-                padding: EdgeInsets.all(16),
-                onPressed: () {
-                  final phone = _phoneController.text.trim();
-
-                  loginUser(phone, context);
-
-                },
-                color: Colors.blue,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//import 'dart:js';
+//
+//import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:flutter/cupertino.dart';
+//import 'package:flutter/material.dart';
+//import 'package:flutter/services.dart';
+//import 'package:health_plus/animation/fade_animation.dart';
+//import 'package:health_plus/ui/registration.dart';
+//
+//class LoginPage extends StatefulWidget {
+//
+//  @override
+//  LoginPageLayout createState() => LoginPageLayout();
+//}
+//
+//
+//class LoginPageLayout extends State<LoginPage> {
+//  final _phoneController = TextEditingController();
+//  final _codeController = TextEditingController();
+//
+//  String phoneNo;
+//  String smsOTP;
+//  String verificationId;
+//  String errorMessage = '';
+//  FirebaseAuth _auth = FirebaseAuth.instance;
+//
+//
+//  Future<bool> smsOTPDialog(BuildContext context) {
+//    return showDialog(
+//        context: context,
+//        barrierDismissible: false,
+//        builder: (BuildContext context) {
+//          return new AlertDialog(
+//            title: Text('Enter SMS Code'),
+//            content: Container(
+//              height: 85,
+//              child: Column(children: [
+//                TextField(
+//                  onChanged: (value) {
+//                    this.smsOTP = value;
+//                  },
+//                ),
+//                (errorMessage != ''
+//                    ? Text(
+//                  errorMessage,
+//                  style: TextStyle(color: Colors.red),
+//                )
+//                    : Container())
+//              ]),
+//            ),
+//            contentPadding: EdgeInsets.all(10),
+//            actions: <Widget>[
+//              FlatButton(
+//                child: Text('Done'),
+//                onPressed: () {
+//                  _auth.currentUser().then((user) {
+//                    if (user != null) {
+//                      Navigator.of(context).pop();
+//                      Navigator.of(context).pushReplacementNamed('/homepage');
+//                    } else {
+//                      signIn();
+//                    }
+//                  });
+//                },
+//              )
+//            ],
+//          );
+//        });
+//  }
+//
+//  handleError(PlatformException error) {
+//    print(error);
+//    switch (error.code) {
+//      case 'ERROR_INVALID_VERIFICATION_CODE':
+//        FocusScope.of(context).requestFocus(new FocusNode());
+//        setState(() {
+//          errorMessage = 'Invalid Code';
+//        });
+//        Navigator.of(context).pop();
+//        smsOTPDialog(context).then((value) {
+//          print('sign in');
+//        });
+//        break;
+//      default:
+//        setState(() {
+//          errorMessage = error.message;
+//        });
+//
+//        break;
+//    }
+//  }
+//
+//
+//  signIn() async {
+//    try {
+//      final AuthCredential credential = PhoneAuthProvider.getCredential(
+//        verificationId: verificationId,
+//        smsCode: smsOTP,
+//      );
+//      final FirebaseUser user = (await _auth.signInWithCredential(credential)) as FirebaseUser;
+//      final FirebaseUser currentUser = await _auth.currentUser();
+//      assert(user.uid == currentUser.uid);
+//      Navigator.of(context).pop();
+//      Navigator.of(context).pushReplacementNamed('/homepage');
+//    } catch (e) {
+//      handleError(e);
+//    }
+//  }
+//  Future<void> verifyPhone() async {
+//    final PhoneCodeSent smsOTPSent = (String verId, [int forceCodeResend]) {
+//      this.verificationId = verId;
+//      smsOTPDialog(context).then((value) {
+//        print('sign in');
+//      });
+//    };
+//    try {
+//      await _auth.verifyPhoneNumber(
+//          phoneNumber: this.phoneNo, // PHONE NUMBER TO SEND OTP
+//          codeAutoRetrievalTimeout: (String verId) {
+//            //Starts the phone number verification process for the given phone number.
+//            //Either sends an SMS with a 6 digit code to the phone number specified, or sign's the user in and [verificationCompleted] is called.
+//            this.verificationId = verId;
+//          },
+//          codeSent:
+//          smsOTPSent, // WHEN CODE SENT THEN WE OPEN DIALOG TO ENTER OTP.
+//          timeout: const Duration(seconds: 20),
+//          verificationCompleted: (AuthCredential phoneAuthCredential) {
+//            print(phoneAuthCredential);
+//          },
+//          verificationFailed: (AuthException exceptio) {
+//            print('${exceptio.message}');
+//          });
+//    } catch (e) {
+//      handleError(e);
+//    }
+//  }
+//  Future<bool> loginUser(String phone, BuildContext context) async{
+//    FirebaseAuth _auth = FirebaseAuth.instance;
+//
+//    _auth.verifyPhoneNumber(
+//        phoneNumber: phone,
+//        timeout: Duration(seconds: 60),
+//        verificationCompleted: (AuthCredential credential) async{
+//          Navigator.of(context).pop();
+//
+//          AuthResult result = await _auth.signInWithCredential(credential);
+//
+//          FirebaseUser user = result.user;
+//
+//          if(user != null){
+//            Navigator.push(context, MaterialPageRoute(
+//                builder: (context) => Registration(user: user,)
+//            ));
+//          }else{
+//            print("Error");
+//          }
+//
+//
+//          //This callback would gets called when verification is done auto maticlly
+//        },
+//        verificationFailed: (AuthException exception){
+//          print(exception);
+//        },
+//        codeSent: (String verificationId, [int forceResendingToken]){
+//          showDialog(
+//              context: context,
+//              barrierDismissible: false,
+//              builder: (context) {
+//                return AlertDialog(
+//                  title: Text("Give the code?"),
+//                  content: Column(
+//                    mainAxisSize: MainAxisSize.min,
+//                    children: <Widget>[
+//                      TextField(
+//                        controller: _codeController,
+//                      ),
+//                    ],
+//                  ),
+//                  actions: <Widget>[
+//                    FlatButton(
+//                      child: Text("Confirm"),
+//                      textColor: Colors.white,
+//                      color: Colors.blue,
+//                      onPressed: () async{
+//                        final code = _codeController.text.trim();
+//                        AuthCredential credential = PhoneAuthProvider.getCredential(verificationId: verificationId, smsCode: code);
+//
+//                        AuthResult result = await _auth.signInWithCredential(credential);
+//
+//                        FirebaseUser user = result.user;
+//
+//                        if(user != null){
+//                          Navigator.push(context, MaterialPageRoute(
+//                              builder: (context) => Registration(user: user,)
+//                          ));
+//                        }else{
+//                          print("Error");
+//                        }
+//                      },
+//                    )
+//                  ],
+//                );
+//              }
+//          );
+//        },
+//        codeAutoRetrievalTimeout: null
+//    );
+//  }
+//
+//
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      backgroundColor: Color.fromRGBO(3, 9, 23, 1),
+//      body: Container(
+//        padding: EdgeInsets.all(30),
+//        child: Column(
+//          crossAxisAlignment: CrossAxisAlignment.start,
+//          mainAxisAlignment: MainAxisAlignment.center,
+//          children: <Widget>[
+//            FadeAnimation(1.2, Text("Enter Your Mobile Number",
+//              style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),)),
+//            SizedBox(height: 30,),
+//            FadeAnimation(1.5, Container(
+//              padding: EdgeInsets.all(10),
+//            )),
+//
+//            TextFormField(
+//              decoration: InputDecoration(
+//                  enabledBorder: OutlineInputBorder(
+//                      borderRadius: BorderRadius.all(Radius.circular(8)),
+//                      borderSide: BorderSide(color: Colors.grey[200])
+//                  ),
+//                  focusedBorder: OutlineInputBorder(
+//                      borderRadius: BorderRadius.all(Radius.circular(8)),
+//                      borderSide: BorderSide(color: Colors.grey[300])
+//                  ),
+//                  filled: true,
+//                  fillColor: Colors.grey[100],
+//                  hintText: "Mobile Number"
+//
+//              ),
+//              controller: _phoneController,
+//            ),
+//            SizedBox(height: 40,),
+//
+//
+//            Container(
+//              width: double.infinity,
+//              child: FlatButton(
+//                child: Text("LOGIN"),
+//                textColor: Colors.white,
+//                padding: EdgeInsets.all(16),
+//                onPressed: () {
+//
+//                  print("test 123");
+//                  final phone = _phoneController.text.trim();
+//
+//                  loginUser(phone, context);
+//
+//                },
+//                color: Colors.red,
+//              ),
+//            ),
+//          ],
+//        ),
+//      ),
+//    );
+//  }
+//}
--- test_otp/lib/main.dart
+++ test_otp/lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'homePage.dart';
+import 'homepage.dart';
 
 void main() => runApp(MyApp());
 
